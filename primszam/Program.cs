725cd41addd06cb7c4a9aecf2d11cc12f4f18553
bluet-it <vince@turcsanyivince.hu>
2020-02-13 18:43:32 +0100

    Add files via upload

---
+++ primszam/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace primszam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i < n; i++)
+            {
+                bool k = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        k = false;
+                    }
+                }
+                if (k)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            while (true)
+            {
+
+            }
+        }
+    }
+}
